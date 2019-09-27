96e3bc9ad23f162f611a6d4988700fbde731a61b
Farhan Tanvir <32593150+Farhandroid@users.noreply.github.com>
2019-09-27 12:26:47 +0900

    pro v1
    pro v1

---
+++ docs/precache-manifest.f7ffdcf1169b1e7078fadc7cae02a7bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99b0d49cb1546bb23c74cbabb0b7d58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c0ab600046cc2b1fdb4",
+    "url": "/static/css/main.ef4104ff.chunk.css"
+  },
+  {
+    "revision": "0424e2a8de76c27099c5",
+    "url": "/static/js/2.b4cef560.chunk.js"
+  },
+  {
+    "revision": "4c0ab600046cc2b1fdb4",
+    "url": "/static/js/main.766a27b0.chunk.js"
+  },
+  {
+    "revision": "cb4fd463e7184abd8f6a",
+    "url": "/static/js/runtime~main.d52357dd.js"
+  }
+]);
